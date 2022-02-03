ec4c53cc6b60b71d822db420e840f88fae2c48be
Alex Riss <00alexx@riss.at>
2022-02-03 14:22:38 +0100

    increase max zoom for filter_overview; add semilicolons

--- res/GUI_zoom_drag.js
+++ res/GUI_zoom_drag.js
@@ -3,25 +3,25 @@
 function zoom_drag_setup(divMain) {
 
     // config
-    let scale = 1  // initial scale
-    const factor = 0.2
-    const max_scale = 10
-    const min_scale = 0.1
+    let scale = 1;  // initial scale
+    const factor = 0.2;
+    const max_scale = 10;
+    const min_scale = 0.1;
 
-    window.scale = scale  // need a global variable here
+    window.scale = scale;  // need a global variable here
 
     // drag the section
     for (const divSection of divMain.getElementsByTagName('section')) {
         // when mouse is pressed store the current mouse x,y
-        let previousX, previousY
+        let previousX, previousY;
         divSection.addEventListener('mousedown', (event) => {
             if (window.sidebar_imagezoomtools && !document.getElementById("line_profile").classList.contains("is-hidden")) {
                 if (!event.shiftKey && !event.ctrlKey && !event.altKey && !window.space_pressed) {  // dragging only with modifier - line profile dragging has priority
                     return;
                 }
             }
-            previousX = event.pageX
-            previousY = event.pageY
+            previousX = event.pageX;
+            previousY = event.pageY;
         })
 
         // when mouse is moved, scrollBy() the mouse movement x,y
@@ -34,21 +34,21 @@ function zoom_drag_setup(divMain) {
                     }
                 }
 
-                let dragX = 0
-                let dragY = 0
+                let dragX = 0;
+                let dragY = 0;
                 // skip the drag when the x position was not changed
                 if (event.pageX - previousX !== 0) {
-                    dragX = previousX - event.pageX
-                    previousX = event.pageX
+                    dragX = previousX - event.pageX;
+                    previousX = event.pageX;
                 }
                 // skip the drag when the y position was not changed
                 if (event.pageY - previousY !== 0) {
-                    dragY = previousY - event.pageY
-                    previousY = event.pageY
+                    dragY = previousY - event.pageY;
+                    previousY = event.pageY;
                 }
                 // scrollBy x and y
                 if (dragX !== 0 || dragY !== 0) {
-                    divMain.scrollBy(dragX, dragY)
+                    divMain.scrollBy(dragX, dragY);
                 }
             }
         })
@@ -63,38 +63,38 @@ function zoom_drag_setup(divMain) {
             if (delta === undefined) {
                 //we are on firefox
                 delta = e.originalEvent.detail;
-            }
-            delta = Math.max(-1, Math.min(1, delta)) // cap the delta to [-1,1] for cross browser consistency
+            };
+            delta = Math.max(-1, Math.min(1, delta)); // cap the delta to [-1,1] for cross browser consistency
             offset = { x: divMain.scrollLeft, y: divMain.scrollTop };
 
             const rect = divMain.getBoundingClientRect();
             image_loc = {
                 x: e.clientX - rect.left + offset.x,
                 y: e.clientY - rect.top + offset.y
-            }
+            };
 
-            zoom_point = { x: image_loc.x / window.scale, y: image_loc.y / window.scale }
+            zoom_point = { x: image_loc.x / window.scale, y: image_loc.y / window.scale };
 
             // apply zoom
-            window.scale += delta * factor * window.scale
-            window.scale = Math.max(min_scale, Math.min(max_scale, window.scale))
+            window.scale += delta * factor * window.scale;
+            window.scale = Math.max(min_scale, Math.min(max_scale, window.scale));
 
             if (window.scale < 1) {
-                divSection.style.transformOrigin = "center"
+                divSection.style.transformOrigin = "center";
             } else {
-                divSection.style.transformOrigin = "0 0"
+                divSection.style.transformOrigin = "0 0";
             }
 
-            zoom_point_new = { x: zoom_point.x * window.scale, y: zoom_point.y * window.scale }
+            zoom_point_new = { x: zoom_point.x * window.scale, y: zoom_point.y * window.scale };
 
             newScroll = {
                 x: zoom_point_new.x - (e.clientX - rect.left),
                 y: zoom_point_new.y - (e.clientY - rect.top)
-            }
+            };
 
-            divSection.style.transform = `scale(${window.scale}, ${window.scale})`
-            divMain.scrollTop = newScroll.y
-            divMain.scrollLeft = newScroll.x
+            divSection.style.transform = `scale(${window.scale}, ${window.scale})`;
+            divMain.scrollTop = newScroll.y;
+            divMain.scrollLeft = newScroll.x;
         }
     })
 
@@ -106,7 +106,7 @@ function zoom_drag_setup(divMain) {
             }
         }
         if (!e.ctrlKey) {
-            zoom_drag_reset(divMain)
+            zoom_drag_reset(divMain);
         }
     })
 }
@@ -114,10 +114,10 @@ function zoom_drag_setup(divMain) {
 function zoom_drag_reset(divMain) {
     window.scale = 1
     for (const divSection of divMain.getElementsByTagName('section')) {
-        divSection.style.transform = "scale(1, 1)"
+        divSection.style.transform = "scale(1, 1)";
     }
-    divMain.scrollTop = 0
-    divMain.scrollLeft = 0
+    divMain.scrollTop = 0;
+    divMain.scrollLeft = 0;
 }
 
 
@@ -128,25 +128,25 @@ function zoom_drag_reset(divMain) {
 function zoom_drag_filter_overview_setup(divMain) {
 
     // config
-    let scale = 1  // initial scale
-    const factor = 0.2
-    const max_scale = 100
-    const min_scale = 1
+    let scale = 1.;  // initial scale
+    const factor = 0.2;
+    const max_scale = 800.0;
+    const min_scale = 1.;
 
     window.scale_filter_overview = scale  // need a global variable here
 
     // drag the section
     for (const divSection of divMain.getElementsByTagName('section')) {
         // when mouse is pressed store the current mouse x,y
-        let previousX, previousY
+        let previousX, previousY;
         divSection.addEventListener('mousedown', (event) => {
             if (window.filter_overview_selecting) {
                 if (!event.altKey && !window.space_pressed) {  // dragging only with modifier - line profile dragging has priority
                     return;
                 }
             }
-            previousX = event.pageX
-            previousY = event.pageY
+            previousX = event.pageX;
+            previousY = event.pageY;
         })
 
         // when mouse is moved, scrollBy() the mouse movement x,y
@@ -159,21 +159,21 @@ function zoom_drag_filter_overview_setup(divMain) {
                     }
                 }
     
-                let dragX = 0
-                let dragY = 0
+                let dragX = 0;
+                let dragY = 0;
                 // skip the drag when the x position was not changed
                 if (event.pageX - previousX !== 0) {
-                    dragX = previousX - event.pageX
-                    previousX = event.pageX
+                    dragX = previousX - event.pageX;
+                    previousX = event.pageX;
                 }
                 // skip the drag when the y position was not changed
                 if (event.pageY - previousY !== 0) {
-                    dragY = previousY - event.pageY
-                    previousY = event.pageY
+                    dragY = previousY - event.pageY;
+                    previousY = event.pageY;
                 }
                 // scrollBy x and y
                 if (dragX !== 0 || dragY !== 0) {
-                    divMain.scrollBy(dragX, dragY)
+                    divMain.scrollBy(dragX, dragY);
                 }
             }
         })
@@ -189,41 +189,41 @@ function zoom_drag_filter_overview_setup(divMain) {
                 //we are on firefox
                 delta = e.originalEvent.detail;
             }
-            delta = Math.max(-1, Math.min(1, delta)) // cap the delta to [-1,1] for cross browser consistency
+            delta = Math.max(-1, Math.min(1, delta)); // cap the delta to [-1,1] for cross browser consistency
             offset = { x: divMain.scrollLeft, y: divMain.scrollTop };
 
             const rect = divMain.getBoundingClientRect();
             image_loc = {
                 x: e.clientX - rect.left + offset.x,
                 y: e.clientY - rect.top + offset.y
-            }
+            };
 
-            zoom_point = { x: image_loc.x / window.scale_filter_overview, y: image_loc.y / window.scale_filter_overview }
+            zoom_point = { x: image_loc.x / window.scale_filter_overview, y: image_loc.y / window.scale_filter_overview };
 
             // apply zoom
-            window.scale_filter_overview += delta * factor * window.scale_filter_overview
-            window.scale_filter_overview = Math.max(min_scale, Math.min(max_scale, window.scale_filter_overview))
+            window.scale_filter_overview += delta * factor * window.scale_filter_overview;
+            window.scale_filter_overview = Math.max(min_scale, Math.min(max_scale, window.scale_filter_overview));
 
             if (window.scale_filter_overview < 1) {
-                divSection.style.transformOrigin = "center"
+                divSection.style.transformOrigin = "center";
             } else {
-                divSection.style.transformOrigin = "0 0"
+                divSection.style.transformOrigin = "0 0";
             }
 
-            zoom_point_new = { x: zoom_point.x * window.scale_filter_overview, y: zoom_point.y * window.scale_filter_overview }
+            zoom_point_new = { x: zoom_point.x * window.scale_filter_overview, y: zoom_point.y * window.scale_filter_overview };
 
             newScroll = {
                 x: zoom_point_new.x - (e.clientX - rect.left),
                 y: zoom_point_new.y - (e.clientY - rect.top)
-            }
+            };
 
             // divSection.style.transform = `scale(${window.scale_filter_overview}, ${window.scale_filter_overview})`
             divSection.style.width = `${100*window.scale_filter_overview}%`;
             divSection.style.height = `${100*window.scale_filter_overview}%`;
     
-            divMain.scrollTop = newScroll.y
-            divMain.scrollLeft = newScroll.x
-            filter_overview_display_coordinates(e)
+            divMain.scrollTop = newScroll.y;
+            divMain.scrollLeft = newScroll.x;
+            filter_overview_display_coordinates(e);
             if (window.scale_filter_overview == 1) {
                 document.getElementById("filter_overview_reset_zoom").classList.add("is-hidden");
             } else {
@@ -240,8 +240,8 @@ function zoom_drag_filter_overview_setup(divMain) {
             }
         }
         if (!e.ctrlKey) {
-            zoom_drag_filter_overview_reset(divMain)
-            filter_overview_display_coordinates(e)
+            zoom_drag_filter_overview_reset(divMain);
+            filter_overview_display_coordinates(e);
         }
     })
 }
@@ -253,8 +253,8 @@ function zoom_drag_filter_overview_reset(divMain) {
         divSection.style.height = "100%";
     }
 
-    divMain.scrollTop = 0
-    divMain.scrollLeft = 0
+    divMain.scrollTop = 0;
+    divMain.scrollLeft = 0;
 
     document.getElementById("filter_overview_reset_zoom").classList.add("is-hidden");
 }
