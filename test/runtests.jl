5d1e0a43a68515cbaf006a0c13b381a63a7a026d
Alex Riss <00alexx@riss.at>
2022-11-19 17:58:21 +0100

    extend wait time for new version check

--- test/runtests.jl
+++ test/runtests.jl
@@ -358,7 +358,7 @@ end
 
 @testset "Check update" begin
     send_click("#modal_about_check_update")
-    sleep(0.5)
+    sleep(0.8)
     no_new_version_hidden = @js w document.getElementById("modal_about_no_new_version").classList.contains("is-hidden")
     new_version_hidden = @js w document.getElementById("modal_about_new_version").classList.contains("is-hidden")
     unreleased_version_hidden = @js w document.getElementById("modal_about_unreleased_version").classList.contains("is-hidden")
